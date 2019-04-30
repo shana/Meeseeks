5235d23dafe919bbc6173435a96950b102c77962
Andreia Gaita <shana@spoiledcat.net>
2019-04-30 15:14:51 +0200

    Clean up the networking bits

--- network.ps1
+++ network.ps1
@@ -1,52 +1,74 @@
-# Description: Malware Analysis Victim VM - Firewall, Defender, Network
-#
-# To install everything, run:
-#   Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/shana/Meeseeks/master/network.ps1'))
-#
+<#
+.SYNOPSIS
+    Malware Analysis Victim VM - Firewall, Defender, Network
+.DESCRIPTION
+    Malware Analysis Victim VM - Firewall, Defender, Network
+.PARAMETER DHCP
+    When true, set network to dhcp
+#>
+[CmdletBinding()]
 
-# Disable Firewall
-Function DisableFirewall {
-    Write-Host "Disabling Firewall..."
-    If (!(Test-Path "HKLM:\SOFTWARE\Policies\Microsoft\WindowsFirewall\StandardProfile")) {
-        New-Item -Path "HKLM:\SOFTWARE\Policies\Microsoft\WindowsFirewall\StandardProfile" -Force | Out-Null
-    }
-    Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\WindowsFirewall\StandardProfile" -Name "EnableFirewall" -Type DWord -Value 0
-}
-DisableFirewall
-# Disable Windows Defender Cloud
-Function DisableDefenderCloud {
-    Write-Host "Disabling Windows Defender Cloud..."
-    If (!(Test-Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows Defender\Spynet")) {
-        New-Item -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows Defender\Spynet" -Force | Out-Null
-    }
-    Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows Defender\Spynet" -Name "SpynetReporting" -Type DWord -Value 0
-    Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows Defender\Spynet" -Name "SubmitSamplesConsent" -Type DWord -Value 2
-}
-DisableDefenderCloud
-# Disable Windows Defender
-Function DisableDefender {
-    Write-Host "Disabling Windows Defender..."
-    If (!(Test-Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows Defender")) {
-        New-Item -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows Defender" -Force | Out-Null
-    }
-    Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows Defender" -Name "DisableAntiSpyware" -Type DWord -Value 1
-    Remove-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Run" -Name "SecurityHealth" -ErrorAction SilentlyContinue
-}
-DisableDefender
+Param(
+    [switch]
+    $DHCP = $false
+)
 
-#--- IP settings ---
-write-output "Configuring Static IP"
-$IPv4Address = "192.168.12.2"
-$IPv4Mask = "255.255.255.0"
-$IPv4Gateway = "192.168.12.1"
-$IPv4DNS = "192.168.12.1"
+New-Module -ScriptBlock {
+
+    # Disable Firewall
+    Function Disable-Firewall {
+        Write-Host "Disabling Firewall..."
+        If (!(Test-Path "HKLM:\SOFTWARE\Policies\Microsoft\WindowsFirewall\StandardProfile")) {
+            New-Item -Path "HKLM:\SOFTWARE\Policies\Microsoft\WindowsFirewall\StandardProfile" -Force | Out-Null
+        }
+        Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\WindowsFirewall\StandardProfile" -Name "EnableFirewall" -Type DWord -Value 0
+    }
+
+    # Disable Windows Defender Cloud
+    Function Disable-DefenderCloud {
+        Write-Host "Disabling Windows Defender Cloud..."
+        If (!(Test-Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows Defender\Spynet")) {
+            New-Item -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows Defender\Spynet" -Force | Out-Null
+        }
+        Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows Defender\Spynet" -Name "SpynetReporting" -Type DWord -Value 0
+        Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows Defender\Spynet" -Name "SubmitSamplesConsent" -Type DWord -Value 2
+    }
+
+    # Disable Windows Defender
+    Function Disable-Defender {
+        Write-Host "Disabling Windows Defender..."
+        If (!(Test-Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows Defender")) {
+            New-Item -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows Defender" -Force | Out-Null
+        }
+        Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows Defender" -Name "DisableAntiSpyware" -Type DWord -Value 1
+        Remove-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Run" -Name "SecurityHealth" -ErrorAction SilentlyContinue
+    }
+
+    Export-ModuleMember -Function Disable-Firewall,Disable-DefenderCloud,Disable-Defender
+}
+
+Disable-Firewall
+Disable-DefenderCloud
+Disable-Defender
 
 $wmi = Get-WmiObject Win32_NetworkAdapterConfiguration -Filter "IpEnabled = 'true'"
-if ($wmi -ne $null) {
-  $wmi.EnableStatic($IPv4Address, $IPv4Mask)
-  $wmi.SetGateways($IPv4Gateway, 1)
-  $wmi.SetDNSServerSearchOrder($IPv4DNS)
+if ($wmi -eq $null) {
+    write-output "error: no active network adapter found"
+    if($myinvocation.mycommand.commandtype -eq 'Script') { return } else { exit -1 }
 }
-else {
-  write-output "error: no active network adapter found"
+
+if ($DHCP) {
+    $wmi.EnableDHCP()
+    $wmi.RenewDHCPLease()
+} else {
+    #--- IP settings ---
+    write-output "Configuring Static IP"
+    $IPv4Address = "192.168.12.2"
+    $IPv4Mask = "255.255.255.0"
+    $IPv4Gateway = "192.168.12.1"
+    $IPv4DNS = "192.168.12.1"
+
+    $wmi.EnableStatic($IPv4Address, $IPv4Mask)
+    $wmi.SetGateways($IPv4Gateway, 1)
+    $wmi.SetDNSServerSearchOrder($IPv4DNS)
 }
