cad286196535fd4125f599a1a8142f8539d081e0
Andreia Gaita <shana@spoiledcat.net>
2019-04-30 15:32:52 +0200

    Make sure networks are set up right

--- network.ps1
+++ network.ps1
@@ -72,4 +72,21 @@ if ($DHCP) {
     $wmi.EnableStatic($IPv4Address, $IPv4Mask)
     $wmi.SetGateways($IPv4Gateway, 1)
     $wmi.SetDNSServerSearchOrder($IPv4DNS)
+
+    $NLMType = [Type]::GetTypeFromCLSID('DCB00C01-570F-4A9B-8D69-199FDBA5723B')
+    $INetworkListManager = [Activator]::CreateInstance($NLMType)
+    $NLM_ENUM_NETWORK_CONNECTED  = 1
+    $NLM_NETWORK_CATEGORY_PUBLIC = 0x00
+    $NLM_NETWORK_CATEGORY_PRIVATE = 0x01
+    $UNIDENTIFIED = "Unidentified network"
+    $INetworks = $INetworkListManager.GetNetworks($NLM_ENUM_NETWORK_CONNECTED)
+    foreach ($INetwork in $INetworks)
+    {
+        $Name = $INetwork.GetName()
+        $Category = $INetwork.GetCategory()
+        if ($INetwork.IsConnected -and ($Category -eq $NLM_NETWORK_CATEGORY_PUBLIC) -and ($Name -eq $UNIDENTIFIED))
+        {
+            $INetwork.SetCategory($NLM_NETWORK_CATEGORY_PRIVATE)
+        }
+    }
 }
